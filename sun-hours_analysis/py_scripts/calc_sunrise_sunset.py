5671ec10f3202fa057f2064295be483977d02225
brades-prox <brades.prox@gmail.com>
2023-09-30 21:32:15 +0200

    MAke changes before I clean things up

--- sun-hours_analysis/py_scripts/calc_sunrise_sunset.py
+++ sun-hours_analysis/py_scripts/calc_sunrise_sunset.py
@@ -5,6 +5,8 @@ from skyfield.api import Topos, load, load_file
 from datetime import datetime, timedelta
 import pytz
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 ts = api.load.timescale()
 eph = api.load('de421.bsp')
@@ -17,13 +19,9 @@ from skyfield import almanac
 Hague = api.wgs84.latlon(+52.0705, 4.3007)
 joburg = api.wgs84.latlon(-26.1223, 28.0115)
 
-
-# johannesburg = Topos(latitude_degrees=-26.2041, longitude_degrees=28.0473)  # Johannesburg
-# hague = Topos(latitude_degrees=52.0705, longitude_degrees=4.3007)
-
-
 t0 = ts.utc(2023, 1, 1, 4)
 t1 = ts.utc(2023, 12, 31, 4)
+
 times_hag, hag_events = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, Hague))
 times_jhb, jhb_events = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, joburg))
 
@@ -32,21 +30,61 @@ times_jhb, jhb_events = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph
 
 # Create a DataFrame
 sunrise_sunset_df = pd.DataFrame({'Sunrise': times_hag.utc_iso()[0::2],
-                                  'Sunset': times_hag.utc_iso()[1::2]})
+                                'Sunset': times_hag.utc_iso()[1::2]})
 
 # Define a function to convert times and calculate time difference
 def convert_and_calculate(df):
+    df['Date'] = pd.to_datetime(df['Sunrise']).dt.date
     df['Sunrise'] = pd.to_datetime(df['Sunrise'])
     df['Sunset'] = pd.to_datetime(df['Sunset'])
-    df['Time Difference (Hours)'] = (df['Sunset'] - df['Sunrise']).dt.total_seconds() / 3600
+    df['Daylight Hours'] = (df['Sunset'] - df['Sunrise']).dt.total_seconds() / 3600
     
-    sunrise_sunset_df['diff_share'] = sunrise_sunset_df['Time Difference (Hours)'] / sunrise_sunset_df['Time Difference (Hours)'].shift(1)
-    sunrise_sunset_df['diff_abs'] = sunrise_sunset_df['Time Difference (Hours)'] - sunrise_sunset_df['Time Difference (Hours)'].shift(1)
-    sunrise_sunset_df['diff_share_minutes'] = sunrise_sunset_df['diff_abs'] * 60
+    df['diff_share'] = df['Daylight Hours'] / df['Daylight Hours'].shift(1)
+    df['diff_abs'] = df['Daylight Hours'] - df['Daylight Hours'].shift(1)
+    df['diff_minutes'] = round(df['diff_abs'] * 60,4)
     return df
+
 
 # Apply the function to the DataFrame
 sunrise_sunset_clean = convert_and_calculate(sunrise_sunset_df)
+
+# plot as line chart
+
+# Plot the data
+# Set the Seaborn style to "whitegrid" for a minimalist look
+sns.set_style("whitegrid")
+
+# Create the first plot
+plot1 = sns.lineplot(data=sunrise_sunset_df, x='Date', y='Daylight Hours')
+plt.title('Daylight Hours Over Time')
+plt.xlabel('Date')
+plt.ylabel('Daylight Hours')
+plt.xticks(rotation=45)
+
+# Create the second plot
+plot2 = sns.lineplot(data=sunrise_sunset_df, x='Date', y='diff_minutes')
+plt.title('Difference in Minutes Over Time')
+plt.xlabel('Date')
+plt.ylabel('Difference in Minutes')
+plt.xticks(rotation=45)
+
+
+
+sns.lineplot(data=sunrise_sunset_df, x='Date', y='Daylight Hours')
+sns.lineplot(data=sunrise_sunset_df, x='Date', y='diff_minutes')
+
+plt.title('Daylight Hours Over Time')
+plt.xlabel('Date')
+plt.ylabel('Daylight Hours')
+
+plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+
+plt.tight_layout()
+plt.show()
+
+
+
+
 
 times_df = times_hag.utc_iso()
 dir(times_hag)
@@ -115,3 +153,31 @@ for t, e in zip(times, events):
     else:
         print(tstr, ' ', almanac.TWILIGHTS[previous_e], 'ends')
     previous_e = e
+
+
+
+#------------------------------------
+
+def calc_sunrise_sunset(lat_long, date_begin, date_end):
+    # Load the ephemeris
+    eph = load('de421.bsp')
+
+    # Retrieve sunrise and sunset times
+    times, events = almanac.find_discrete(date_begin, date_end, almanac.sunrise_sunset(eph, lat_long))
+
+    # Create a DataFrame
+    sunrise_sunset_df = pd.DataFrame({'Sunrise': times.utc_iso()[0::2],
+                                        'Sunset': times.utc_iso()[1::2]})
+
+    # Convert times and calculate time difference
+    sunrise_sunset_df['Date'] = pd.to_datetime(sunrise_sunset_df['Sunrise']).dt.date # add date
+    sunrise_sunset_df['Sunrise'] = pd.to_datetime(sunrise_sunset_df['Sunrise']) #specify sunrise time
+    sunrise_sunset_df['Sunset'] = pd.to_datetime(sunrise_sunset_df['Sunset']) #specify sunset time
+    sunrise_sunset_df['Daylight Hours'] = (sunrise_sunset_df['Sunset'] - sunrise_sunset_df['Sunrise']).dt.total_seconds() / 3600 # calc daylight hours 
+    sunrise_sunset_df['daily_delta_share'] = sunrise_sunset_df['Daylight Hours'] / sunrise_sunset_df['Daylight Hours'].shift(1)
+    sunrise_sunset_df['daily_delta_hours'] = sunrise_sunset_df['Daylight Hours'] - sunrise_sunset_df['Daylight Hours'].shift(1)
+    sunrise_sunset_df['daily_delta_minutes'] = round(sunrise_sunset_df['daily_delta_hours'] * 60, 4)
+
+    return sunrise_sunset_df
+
+calc_sunrise_sunset(Hague, t0, t1)
